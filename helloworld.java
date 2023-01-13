50608c28c68c3e322b3291ba68f1abfdba31e813
vinatadanki <95192885+vinatadanki@users.noreply.github.com>
2023-01-13 16:31:40 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World -at next min");
+    System.out.println("Hello World -at next minute ");
     }
 }
